f0e1ec740299942d67ec788b1044e4a9e822febe
Martin Nikolov <martooo3000@gmail.com>
2022-05-02 15:33:27 +0300

    new commite

--- functions.cpp
+++ functions.cpp
@@ -4,4 +4,9 @@
 using namespace std;
 //file location: "C:/Users/ACER/source/repos/fmi project (1)/file.txt" !!!REPLACE EVERYWHERE WHEN USING FILE WITH DIFFERENT PATH!!!
 
-//test push
+User::User()
+{
+	ofstream writeNewUser("C:/Users/ACER/source/repos/fmi project (1)/file.txt", ios::out);
+	username = new char[13];
+	strcpy_s(username, 13, "default name");
+}
--- header.h
+++ header.h
@@ -2,6 +2,16 @@
 #define HEADER
 class User
 {
+	char* username;
+	char* pasword;
+	char* email;
+	Destination* destinations;
+public:
+	User();
+	~User();
+};
+class Destination
+{
 
 };
 #endif // !HEADER
