831479c9735383dec8b6192348bee051ce394c67
Martin Nikolov <martooo3000@gmail.com>
2022-05-02 16:20:34 +0300

    new commite

--- functions.cpp
+++ functions.cpp
@@ -25,9 +25,38 @@ User::User()
 	writeNewUser.write((char*)&leng, sizeof(leng));
 	writeNewUser.write(email, leng).write("\n", 1);
 
+	writeNewUser.close();
+
 	//cout << username << "\n";
 	//cout << password << "\n";
 	//cout << email << "\n";
+}
+void User::giveUser()
+{
+	ifstream giveUser("C:/Users/ACER/source/repos/fmi project (1)/file.txt", ios::in);
+	int leng = 0;
+	giveUser.read((char*)&leng, sizeof(leng));
+	char* give1 = new char[leng];
+	giveUser.read(give1, leng);
+	cout << give1 << "\n";
+
+	int pos = giveUser.tellg();
+	giveUser.seekg(pos += 2);
+	giveUser.read((char*)&leng, sizeof(leng));
+	char* give2 = new char[leng];
+	giveUser.read(give2, leng);
+	cout << give2 << "\n";
+
+	pos = giveUser.tellg();
+	giveUser.seekg(pos += 2);
+	giveUser.read((char*)&leng, sizeof(leng));
+	char* give3 = new char[leng];
+	giveUser.read(give3, leng);
+	cout << give3 << "\n";
+
+	delete[] give1;
+	delete[] give2;
+	delete[] give3;
 }
 User::~User()
 {
--- header.h
+++ header.h
@@ -9,6 +9,7 @@ class User
 public:
 	User();
 	~User();
+	void giveUser();
 };
 class Destination
 {
