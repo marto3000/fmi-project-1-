ef28d19b5cb730262d8429c0a970a465a0e68b92
Martin Nikolov <martooo3000@gmail.com>
2022-05-08 20:39:13 +0300

    new commite (swapped the email and username location, changed from email to username search in the login function)

--- functions.cpp
+++ functions.cpp
@@ -2,27 +2,41 @@
 #include <fstream>
 #include "header.h"
 using namespace std;
-//files locations: "C:/Users/ACER/source/repos/fmi project (1)/UsersLoginPosition.txt"			!!!REPLACE EVERYWHERE WHEN USING FILE WITH DIFFERENT PATH!!!
-//				   "C:/Users/ACER/source/repos/fmi project (1)/UsersLoginData.txt"				!!!REPLACE EVERYWHERE WHEN USING FILE WITH DIFFERENT PATH!!!
-//				   "C:/Users/ACER/source/repos/fmi project (1)/UsersLocationsData.txt"			!!!REPLACE EVERYWHERE WHEN USING FILE WITH DIFFERENT PATH!!!
+//files locations: "C:/Users/ACER/source/repos/fmi project (1)/UsersLoginData.txt"		!!!REPLACE EVERYWHERE WHEN USING FILE WITH DIFFERENT PATH!!!
+//				   "C:/Users/ACER/source/repos/fmi project (1)/UsersLocationsData.txt"	!!!REPLACE EVERYWHERE WHEN USING FILE WITH DIFFERENT PATH!!!
 				   
 User::User()//not logged default user
 {
-	email = new char[15];//email length;
-	strcpy_s(email, 15, "default_email5");
+	username = new char[14];//name length
+	strcpy_s(username, 14, "default_name4");
 
 	password = new char[18];//password length
-	strcpy_s(password, 18, "default_password5");
-	
-	username = new char[14];//name length
-	strcpy_s(username, 14, "default_name5");
+	strcpy_s(password, 18, "default_password4");
 
-	destCount = 5;
+	email = new char[15];//email length;
+	strcpy_s(email, 15, "default_email4");
 
-	//cout << email << "\n";
+	destCount = 0;
+
+	//cout << username << "\n";s
 	//cout << password << "\n";
-	//cout << username << "\n";
+	//cout << email << "\n";
 	//cout << destCount << "\n";
+}
+void User::registration()
+{
+	//clearing the user loaded data
+	username = new char[1];
+	username[0] = '\0';
+	password = new char[1];
+	password[0] = '\0';
+	email = new char[1];
+	email[0] = '\0';
+	//getting information
+	cout << "Enter your email: ";
+	char* getData = new char[1024];
+
+	delete[] getData;
 }
 void User::writeUser()//for saving new users in the end of the file (after the last user)
 {
@@ -30,18 +44,18 @@ void User::writeUser()//for saving new users in the end of the file (after the l
 	ofstream writeNewUser("C:/Users/ACER/source/repos/fmi project (1)/UsersLoginData.txt", ios::app);
 	int maxLeng = strlen(username) + 1 + strlen(password) + 1 + strlen(email) + 1 + sizeof(destCount) + (3 * 4) + (4 * 2);//each char array + "\0" from each array + the destination count + every number telling the array size + every "\n"
 	writeNewUser.write((char*)&maxLeng, sizeof(maxLeng));
-	//writing email
-	int leng = strlen(email) + 1;
+	//writing username
+	int leng = strlen(username) + 1;
 	writeNewUser.write((char*)&leng, sizeof(leng));
-	writeNewUser.write(email, leng).write("\n", 1);
+	writeNewUser.write(username, leng).write("\n", 1);
 	//writing password
 	leng = strlen(password) + 1;
 	writeNewUser.write((char*)&leng, sizeof(leng));
 	writeNewUser.write(password, leng).write("\n", 1);
-	//writing username
-	leng = strlen(username) + 1;
+	//writing email
+	leng = strlen(email) + 1;
 	writeNewUser.write((char*)&leng, sizeof(leng));
-	writeNewUser.write(username, leng).write("\n", 1);
+	writeNewUser.write(email, leng).write("\n", 1);
 	//writing destCount
 	writeNewUser.write((char*)&destCount, sizeof(destCount)).write("\n", 1);
 	writeNewUser.close();
@@ -51,22 +65,22 @@ void User::login()
 	bool isLogged = false;//default_email1 default_password1
 	while (isLogged == false)
 	{
-		char* getEmail = new char[1024];
+		char* getUser = new char[1024];
 		char* getPass = new char[1024];
-		cout << "Enter your email and password: ";
-		cin >> getEmail;
+		cout << "Enter your username and password: ";
+		cin >> getUser;
 		cin >> getPass;
 		getPass[strlen(getPass)] = '\0';
-		char* email = new char[strlen(getEmail) + 1];
-		strcpy_s(email, strlen(getEmail) + 1, getEmail);
-		email[strlen(getEmail)] = '\0';
+		char* user = new char[strlen(getUser) + 1];
+		strcpy_s(user, strlen(getUser) + 1, getUser);
+		user[strlen(getUser)] = '\0';
 		char* pass = new char[strlen(getPass) + 1];
 		strcpy_s(pass, strlen(getPass) + 1, getPass);
 		pass[strlen(getPass)] = '\0';
-		delete[] getEmail;
+		delete[] getUser;
 		delete[] getPass;
-		isLogged = giveUser(email, pass);
-		delete[] email;
+		isLogged = giveUser(user, pass);
+		delete[] user;
 		delete[] pass;
 		if (isLogged == false)
 		{
@@ -97,7 +111,7 @@ bool User::giveUser(char* name, char* pass)//from the file
 		//when the end of the file is reached
 		if (giveUser.tellg() < 0)
 		{
-			cout << "Wrong email or password.";
+			cout << "Wrong username or password.";
 			i = true;
 			return false;
 		}
@@ -105,7 +119,7 @@ bool User::giveUser(char* name, char* pass)//from the file
 		{
 			int skipFrom = giveUser.tellg();
 			giveUser.read((char*)&leng, sizeof(leng));
-			//checking the email
+			//checking the username
 			if (leng == strlen(name) + 1)
 			{
 				char* give1 = new char[leng];
@@ -119,7 +133,7 @@ bool User::giveUser(char* name, char* pass)//from the file
 						charPos = leng;
 						delete[] give1;
 					}
-					//checking the password if the email is correct (must be after the "on first wrong letter")
+					//checking the password if the username is correct (must be after the "on first wrong letter")
 					if (charPos == leng - 1)
 					{
 						i = true;
@@ -140,16 +154,16 @@ bool User::giveUser(char* name, char* pass)//from the file
 									charPos2 = leng2;
 									delete[] give2;
 									//wrong password
-									cout << "Wrong email or password.";
+									cout << "Wrong username or password.";
 									return false;
 								}
 								//if the password is correct
 								if (charPos2 == leng2 - 1)
 								{
-									//giving email
-									email = new char[strlen(give1) + 1];
-									strcpy_s(email, strlen(give1) + 1, give1);
-									email[strlen(give1)] = '\0';
+									//giving username
+									username = new char[strlen(give1) + 1];
+									strcpy_s(username, strlen(give1) + 1, give1);
+									username[strlen(give1)] = '\0';
 									//cout << email << "\n";//comment or delete later
 									//giving password
 									password = new char[strlen(give2) + 1];
@@ -163,9 +177,9 @@ bool User::giveUser(char* name, char* pass)//from the file
 									giveUser.read((char*)&leng2, sizeof(leng2));
 									char* give3 = new char[leng2];
 									giveUser.read(give3, leng2);
-									username = new char[strlen(give3) + 1];
-									strcpy_s(username, strlen(give3) + 1, give3);
-									username[strlen(give3)] = '\0';
+									email = new char[strlen(give3) + 1];
+									strcpy_s(email, strlen(give3) + 1, give3);
+									email[strlen(give3)] = '\0';
 									//cout << username << "\n";//comment or delete later
 									//giving destCount
 									pos = giveUser.tellg();
@@ -187,7 +201,7 @@ bool User::giveUser(char* name, char* pass)//from the file
 						//shorter/longer password
 						else
 						{
-							cout << "Wrong email or password.";
+							cout << "Wrong username or password.";
 							return false;
 						}
 					}
--- header.h
+++ header.h
@@ -2,14 +2,15 @@
 #define HEADER
 class User
 {
-	char* email;
-	char* password;
 	char* username;
+	char* password;
+	char* email;
 	//Destination* destinations;
 	int destCount;
 public:
 	User();
 	~User();
+	void registration();
 	void writeUser();
 	void login();
 	bool giveUser(char* name, char* pass);
